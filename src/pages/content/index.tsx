21765691ceae35fcc90d302bc02c0d851eccf868
Command <me@c0mm4nd.com>
2025-01-05 16:13:12 +0800

    init kyt

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,16 +1,35 @@
 import { createRoot } from 'react-dom/client';
-import './style.css' 
+
+// eslint-disable-next-line
+// @ts-ignore
+import tailwindcssOutput from '../../assets/styles/tailwind-inline.css?inline';
+
+/** Inject styles into shadow dom */
+const globalStyleSheet = new CSSStyleSheet();
+globalStyleSheet.replaceSync(tailwindcssOutput);
+
 const div = document.createElement('div');
 div.id = '__root';
 document.body.appendChild(div);
+const shadowRoot = div.attachShadow({ mode: 'open' });
 
-const rootContainer = document.querySelector('#__root');
-if (!rootContainer) throw new Error("Can't find Content root element");
+
+// const rootContainer = document.querySelector('#__root');
+
+// if (!rootContainer) throw new Error("Can't find Content root element");
+const rootContainer = document.createElement('div');
+rootContainer.id = 'kyt-ext-root';
+shadowRoot.adoptedStyleSheets = [globalStyleSheet];
+shadowRoot.appendChild(rootContainer);
+
 const root = createRoot(rootContainer);
+
 root.render(
-  <div className='absolute bottom-0 left-0 text-lg text-black bg-amber-400 z-50'  >
-    content script <span className='your-class'>loaded</span>
+  // <CacheProvider value={emotionCache}>
+  <div >
+    content script <span className='text-blue-900 text-bold'>loaded</span>
   </div>
+  // </CacheProvider>
 );
 
 try {
